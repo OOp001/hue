7e08cdc6b10a4e5f9a4913e644463777b8cd0617
Ying Chen <yingchen@cloudera.com>
2019-07-15 15:05:54 -0700

    HUE-8737 [core] Futurize desktop/libs/indexer for Python 3.5

--- desktop/libs/indexer/src/indexer/indexers/sql.py
+++ desktop/libs/indexer/src/indexer/indexers/sql.py
@@ -14,8 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.import logging
 
+from future import standard_library
+standard_library.install_aliases()
+from builtins import object
 import logging
-import urllib
+import sys
+import urllib.request, urllib.error
 
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -24,13 +28,17 @@ from django.utils.translation import ugettext as _
 from desktop.lib import django_mako
 from notebook.models import make_notebook
 
+if sys.version_info[0] > 2:
+  from urllib.parse import unquote as urllib_unquote
+else:
+  from urllib import unquote as urllib_unquote
 
 LOG = logging.getLogger(__name__)
 
 
 try:
   from beeswax.server import dbms
-except ImportError, e:
+except ImportError as e:
   LOG.warn('Hive and HiveServer2 interfaces are not enabled')
 
 
@@ -56,9 +64,9 @@ class SQLIndexer(object):
     kudu_partition_columns = destination['kuduPartitionColumns']
     comment = destination['description']
 
-    source_path = urllib.unquote(source['path'])
+    source_path = urllib_unquote(source['path'])
     external = not destination['useDefaultLocation']
-    external_path = urllib.unquote(destination['nonDefaultLocation'])
+    external_path = urllib_unquote(destination['nonDefaultLocation'])
 
     load_data = destination['importData']
     skip_header = destination['hasHeader']
@@ -164,7 +172,8 @@ class SQLIndexer(object):
         'overwrite': False,
         'partition_columns': [(partition['name'], partition['partitionValue']) for partition in partition_columns],
       }
-      db = dbms.get(self.user)
+      query_server_config = dbms.get_query_server_config(name=source_type)
+      db = dbms.get(self.user, query_server=query_server_config)
       sql += "\n\n%s;" % db.load_data(database, table_name, form_data, None, generate_ddl_only=True)
 
     if load_data and table_format in ('parquet', 'kudu'):
